e6490b6d71bf4e47787b20e29040a3f91fbe033a
AmirrezaAsk <raskarpour@gmail.com>
2022-04-30 19:57:05 +0430

    parsing a number declaration

--- src/parser.rs
+++ src/parser.rs
@@ -1,24 +1,11 @@
 // Vec<Token> -> Module
 use crate::tokenizer::{Token, TokenType};
 use crate::tokenizer::Errors;
-#[derive(Copy, Clone, PartialEq, Eq, Debug)]
-pub enum ExprType {
-    Fn,
-    Int,
-    Float32,
-    Float64,
-    String,
-    Char,
-    If,
-}
+
 #[derive(Clone, Copy, Eq, PartialEq, Debug)]
-enum Payload {
+pub enum Expr {
     Int(i64),
-}
-#[derive(Clone, Copy, Eq, PartialEq, Debug)]
-pub struct Expr {
-    ty: ExprType,
-    payload: Option<Payload>,
+    Nil,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -28,7 +15,7 @@ pub struct Decl {
 }
 
 pub fn parse(tokens: Vec<Token>) -> Result<Vec<Decl>, Errors> {
-    let decls = vec![];
+    let mut decls = vec![];
     let mut current_decl: Option<Decl> = None;
 
     for (i, tok) in tokens.iter().enumerate() {
@@ -36,17 +23,18 @@ pub fn parse(tokens: Vec<Token>) -> Result<Vec<Decl>, Errors> {
             if current_decl.is_none() {
                 current_decl = Some(Decl {
                     name: tok.value.as_ref().unwrap().to_string(), // TODO: fix this, do not unwrap
-                    expr: Expr {
-                        ty: ExprType::Int,
-                        payload: None,
-                    }
+                    expr: Expr::Nil
                 });
             }
         } else if tok.ty == TokenType::AssignOp {
             let next_tok = tokens.iter().nth(i+1).unwrap();
             if next_tok.ty == TokenType::Number {
                 let number:i64 = next_tok.value.as_ref().unwrap().parse().unwrap();
-                current_decl.unwrap().expr.payload = Some(Payload::Int(number));
+                current_decl.as_mut().unwrap().expr = Expr::Int(number);
+            }
+        } else if tok.ty == TokenType::SemiColon {
+            if let Some(decl) = current_decl.clone() {
+                decls.push(decl.clone());
             }
         }
 
@@ -62,7 +50,9 @@ pub fn parse(tokens: Vec<Token>) -> Result<Vec<Decl>, Errors> {
 mod tests {
     use super::Token;
     use super::TokenType;
+    use super::Expr;
     use super::parse;
+    use super::Decl;
     fn eq_vecs<T: Eq + std::fmt::Debug>(v1: Vec<T>, v2: Vec<T>) -> bool {
         if v1.len() != v2.len() {
             assert_eq!(v1.len(), v2.len());
@@ -91,12 +81,19 @@ mod tests {
             Token {
                 ty: TokenType::Number,
                 value: Some(String::from("12")),
+            },
+            Token {
+                ty: TokenType::SemiColon,
+                value: None,
             }
         ];
 
         let decls = parse(tokens).unwrap();
 
-        eq_vecs(decls, vec![]);
+        eq_vecs(decls, vec![Decl{
+            name: "x".to_string(),
+            expr: super::Expr::Int(12),
+        }]);
 
 
     }
