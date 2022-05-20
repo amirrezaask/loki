6d3e0599b6170ec4f8274781953f4653367001a0
AmirrezaAsk <raskarpour@gmail.com>
2022-05-20 23:59:05 +0430

    import, @imports

--- src/passes/imports.rs
+++ src/passes/imports.rs
@@ -1,8 +1,9 @@
-use crate::parser::{module, Node};
+use crate::parser::{module, Node, Import};
 use anyhow::Result;
 
 fn parse_module(path: &str) -> Result<Node> {
     let (_, parsed_module) = module(std::fs::read_to_string(path)?)?;
+    let parsed_module = include_imports(parsed_module)?;
     Ok(parsed_module)
 }
 
@@ -12,7 +13,13 @@ pub fn include_imports(mut ast: Node) -> Result<Node> {
             for stmt in stmts.iter_mut() {
                 if let Node::Import(i) = stmt {
                     if let Node::Str(path) = &i.path {
-                        if !path.contains("c:") {
+                        println!("import {}", path);
+                        if path.chars().next().unwrap() == '@' {
+                            *stmt = Node::Import(Box::new(Import {
+                                path: Node::Str(path[1..].to_string()),
+                                _as: None,
+                            }))
+                        } else {
                             let m = parse_module(path.as_str())?;
                             *stmt = m;
                         }
