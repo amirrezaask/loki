932e86c87580500c74c6b3ba78637bda94a39acb
AmirrezaAsk <raskarpour@gmail.com>
2022-07-10 00:29:37 +0430

    some basic work on Ast

--- src/Ast.zig
+++ src/Ast.zig
@@ -1,2 +1,51 @@
 const std = @import("std");
 const Self = @This();
+
+pub const Node = struct {
+    pub const Decl = struct {
+        ty: enum {
+            @"const",
+            @"var",
+        },
+        name: []const u8,
+        val: Expr,
+    };
+    pub const Import = struct {
+        path: []const u8,
+    };
+
+    pub const Struct = struct {};
+    pub const Union = struct {};
+    pub const Enum = struct {};
+    pub const FnCall = struct {};
+    pub const Expr = union(enum) {
+        @"int": i64,
+        @"float": f64,
+        @"string": []const u8,
+        @"bool": bool,
+        @"identifier": []const u8,
+        paren_expr: Expr,
+        type_def: union(enum) {
+            @"struct": Struct,
+            @"enum": Enum,
+            @"union": Union,
+        },
+        increment: Expr,
+        decrement: Expr,
+        call: FnCall,
+    };
+    pub const Type = enum {
+        @"decl",
+        @"import",
+        @"expr",
+    };
+    pub const Val = union(enum) {
+        @"const_decl": ConstDecl,
+        @"var_decl": VarDecl,
+        @"import": Import,
+        @"expr": Expr,
+    };
+    ty: Type,
+};
+
+top_level: []Node,
